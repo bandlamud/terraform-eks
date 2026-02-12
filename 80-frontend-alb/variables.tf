8a4b7547703122c5fa545ef8a6dd8261f827e842
bharathchandu302 <bharathchandu302@gmail.com>
2026-02-12 15:54:13 +0530

    terraform

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00363011Z6DCTPYPRFG8"
+}
+
+variable "domain_name" {
+    default = "daws89s.fun"
+}
---
+++ 90-eks/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00363011Z6DCTPYPRFG8"
+}
+
+variable "domain_name" {
+    default = "daws89s.fun"
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
